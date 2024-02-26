8e142255311e4615d67d6d1dc45f3eb7a3cce633
csnizik <csnizik@tulane.edu>
2024-02-26 16:33:36 -0600

    oops

--- components/Card.js
+++ components/Card.js
@@ -4,9 +4,7 @@ import Link from './Link'
 const Card = ({ title, description, imgSrc, href }) => (
   <div className="md p-4 md:w-1/2">
     <div
-      className={`${
-        imgSrc && 'h-full'
-      } overflow-hidden rounded-md border-2 border-gray-200 border-opacity-60 dark:border-gray-700`}
+      className={`overflow-hidden rounded-md border-2 border-gray-200 border-opacity-60 dark:border-gray-700`}
     >
       {imgSrc && (
         <div className="relative w-full pt-3">
