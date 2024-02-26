404fb47e463e49f779932381368da55adc2b2783
csnizik <csnizik@tulane.edu>
2024-02-26 15:56:23 -0600

    adding TS temp page

---
+++ data/tsListingsData.js
@@ -0,0 +1,39 @@
+const projectsData = [
+  {
+    title: `Hippity Hoppity...`,
+    description: `Size: 104 x 169 | 6,530 stitches | 2,418 backstitches | 20 colors`,
+    imgSrc: '/static/images/tslistings/hippity-product-image-4d.png',
+    href: 'https://www.etsy.com/listing/1668242140/hippity-hoppity-get-off-my-property?click_key=aeee6eeb527ef310a70a2b8dd571dfab21809ab5%3A1668242140&click_sum=a2595f2a&ref=shop_home_feat_1&pro=1',
+  },
+  {
+    title: 'Every Time...',
+    description: `Size: 119 x 125 | 6,891 stitches | 2,434 backstitches | 22 colors`,
+    imgSrc: '/static/images/tslistings/stab-product-image-4d.png',
+    href: 'https://www.etsy.com/listing/1671142496/every-time-i-stab-my-finger-cross-stitch?click_key=b36b91e46ca56ab20dbccdb8b76bb1509a78ae35%3A1671142496&click_sum=9db9b1c1&ref=shop_home_active_1&pro=1',
+  },
+  {
+    title: 'Every Time...',
+    description: `Size: 139 x 151 | 14,036 stitches | 592 backstitches | 47 colors`,
+    imgSrc: '/static/images/tslistings/iwillnot-product-image-4d.png',
+    href: 'https://www.etsy.com/listing/1685107767/i-will-not-cross-stitch-pattern-pdf?click_key=30690da6fe0bf8252a2632a91daae60bb77955da%3A1685107767&click_sum=2a424835&ref=shop_home_active_2&pro=1',
+  },
+  {
+    title: 'Such a Big World...',
+    description: `Size: 212 x 235 | 25,964 stitches | 3,168 backstitches | 64 colors`,
+    imgSrc: '/static/images/tslistings/dipshits-product-image-4d.png',
+    href: 'https://www.etsy.com/listing/1684705503/so-many-dipshits-cross-stitch-pattern?click_key=db08ab2c951ab574044094621254137ce22ee0b3%3A1684705503&click_sum=b5512744&ref=shop_home_active_3&pro=1',
+  },
+  {
+    title: '...the Shit Show',
+    description: `Size: 134 x 144 | 11,773 stitches | 2,166 backstitches | 54 colors`,
+    imgSrc: '/static/images/tslistings/shitshow-product-image-4d.png',
+    href: 'https://www.etsy.com/listing/1684071975/on-with-the-shit-show-cross-stitch?click_key=011af642052cf1ac7631d4c7107b92692468cf7d%3A1684071975&click_sum=e8d0f92b&ref=shop_home_active_4&pro=1',
+  },
+  {
+    title: `I'm Not Always...`,
+    description: `Size: 149x 230 | 23,483 stitches | 1,572 backstitches | 48 colors`,
+    imgSrc: '/static/images/tslistings/shitshow-product-image-4d.png',
+    href: 'https://www.etsy.com/listing/1683546499/im-not-always-a-bitch-cross-stitch?click_key=d257e5dd1aee85de1b445a23713eab4d2a8941db%3A1683546499&click_sum=c35f5e35&ref=shop_home_active_5&pro=1',
+  },
+]
+export default projectsData
--- pages/tabascosunrise.js
+++ pages/tabascosunrise.js
@@ -1,7 +1,7 @@
 import siteMetadata from '@/data/siteMetadata'
-import projectsData from '@/data/projectsData'
-import Card from '@/components/Card'
 import { PageSEO } from '@/components/SEO'
+import Card from '@/components/Card'
+import tsListingsData from '@/data/tsListingsData' // Ensure this path is correct
 
 export default function TabascoSunrise() {
   return (
@@ -10,29 +10,26 @@ export default function TabascoSunrise() {
         title={`Tabasco Sunrise - ${siteMetadata.author}`}
         description={siteMetadata.description}
       />
-      <div className="divide-y divide-gray-200 dark:divide-gray-700">
-        <div className="space-y-2 pt-6 pb-8 md:space-y-5">
-          <h1 className="text-3xl font-extrabold leading-9  text-gray-900 dark:text-gray-100 sm:text-4xl sm:leading-10 md:text-6xl md:leading-14">
-            Tabasco Sunrise
-          </h1>
-          <p className="text-lg leading-7 text-gray-500 dark:text-gray-400">
-            One day, I'll fill this space with wonderful projects I've worked on. Until then, enjoy
-            these placeholders.
-          </p>
+      <div className="flex flex-col items-center justify-center py-10">
+        <img
+          src="/static/images/ts-logo-200x200.png"
+          alt="Tabasco Sunrise Logo"
+          className="w-200 h-200"
+        />
+      </div>
+      <div className="container mx-auto">
+        <div className="grid grid-cols-1 gap-4 sm:grid-cols-2 lg:grid-cols-3">
+          {tsListingsData.map((listing) => (
+            <Card
+              key={listing.id} // Assuming each listing has a unique 'id'
+              title={listing.title}
+              description={listing.description}
+              imgSrc={listing.imgSrc}
+              href={listing.href}
+              className="border-[#9b1b22] bg-[#057342] text-[#fcd23b] hover:bg-[#9b1b22] hover:text-white"
+            />
+          ))}
         </div>
-        {/* <div className="container py-12">
-          <div className="-m-4 flex flex-wrap">
-            {projectsData.map((d) => (
-              <Card
-                key={d.title}
-                title={d.title}
-                description={d.description}
-                imgSrc={d.imgSrc}
-                href={d.href}
-              />
-            ))}
-          </div>
-        </div> */}
       </div>
     </>
   )
